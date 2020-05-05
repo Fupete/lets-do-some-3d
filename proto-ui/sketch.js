2382d6b762c38a907e0255a127ff60c93ec3ff12
dani-fupete-teller <teller@tellerk.com>
2020-05-05 17:31:23 +0200

    test

--- proto-ui/sketch.js
+++ proto-ui/sketch.js
@@ -202,9 +202,9 @@ let render = function() {
   camera.rotation.y = gimbal.yaw
   camera.rotation.z = gimbal.roll
   // camera.quaternion.copy(gimbal.quaternion)
-  camera.position.x = ( mouse.x - camera.position.x ) * .005
-	camera.position.y = ( - mouse.y - camera.position.y ) * .005
-	camera.lookAt( scene.position )
+  // camera.position.x = ( mouse.x - camera.position.x ) * .005
+	// camera.position.y = ( - mouse.y - camera.position.y ) * .005
+	// camera.lookAt( scene.position )
 
   renderer.render(scene, camera)
   // time += 0.015
